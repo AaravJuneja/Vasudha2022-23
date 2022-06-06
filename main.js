09f87ea277d72794e5a540c0e8a9e3a3df7410eb
Aarav Juneja <aarav.juneja@ais.amity.edu>
2022-06-06 12:18:46 +0530

    Last Commit

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCEc9KL1JLJQyLWqYMn5kO6OytbPvDLOKw",
-  authDomain: "vasudha-2022-23.firebaseapp.com",
-  projectId: "vasudha-2022-23",
-  storageBucket: "vasudha-2022-23.appspot.com",
-  messagingSenderId: "333093383398",
-  appId: "1:333093383398:web:f95d3c52b54b4a333a9f27",
-  measurementId: "G-4K6MFPLE4C"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-async function getCities(db) {
-const citiesCol = collection(db, 'cities');
-const citySnapshot = await getDocs(citiesCol);
-const cityList = citySnapshot.docs.map(doc => doc.data());
-return cityList;
-}
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -32,250 +7,32 @@ function myFunction() {
     }
   }
 
-  import { getAuth } from "firebase/auth";
+function login() {
+  var userEmail = document.getElementById("email-field").value;
+  var userPass = document.getElementById("email-field").value;
 
-  const auth = getAuth();
-  auth.languageCode = 'it';
-  // To apply the default browser preference instead of explicitly setting it.
-  // firebase.auth().useDeviceLanguage();
+  window.alert(userEmail + " " + userPass);
+}
 
-  import { getAuth, RecaptchaVerifier } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const auth = getAuth();
-window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
-  'size': 'invisible',
-  'callback': (response) => {
-    // reCAPTCHA solved, allow signInWithPhoneNumber.
-    onSignInSubmit();
-  }
-}, auth);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCEc9KL1JLJQyLWqYMn5kO6OytbPvDLOKw",
+  authDomain: "vasudha-2022-23.firebaseapp.com",
+  databaseURL: "https://vasudha-2022-23-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vasudha-2022-23",
+  storageBucket: "vasudha-2022-23.appspot.com",
+  messagingSenderId: "333093383398",
+  appId: "1:333093383398:web:f95d3c52b54b4a333a9f27",
+  measurementId: "G-4K6MFPLE4C"
+};
 
-import { getAuth, RecaptchaVerifier } from "firebase/auth";
-
-const auth = getAuth();
-window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
-
-import { getAuth, RecaptchaVerifier } from "firebase/auth";
-
-const auth = getAuth();
-window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-  'size': 'normal',
-  'callback': (response) => {
-    // reCAPTCHA solved, allow signInWithPhoneNumber.
-    // ...
-  },
-  'expired-callback': () => {
-    // Response expired. Ask user to solve reCAPTCHA again.
-    // ...
-  }
-}, auth);
-
-recaptchaVerifier.render().then((widgetId) => {
-  window.recaptchaWidgetId = widgetId;
-});
-
-const recaptchaResponse = grecaptcha.getResponse(recaptchaWidgetId);
-
-import { getAuth, signInWithPhoneNumber } from "firebase/auth";
-
-const phoneNumber = getPhoneNumberFromUserInput();
-const appVerifier = window.recaptchaVerifier;
-
-const auth = getAuth();
-signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-      // SMS sent. Prompt user to type the code from the message, then sign the
-      // user in with confirmationResult.confirm(code).
-      window.confirmationResult = confirmationResult;
-      // ...
-    }).catch((error) => {
-      // Error; SMS not sent
-      // ...
-    });
-
-    grecaptcha.reset(window.recaptchaWidgetId);
-
-// Or, if you haven't stored the widget ID:
-window.recaptchaVerifier.render().then(function(widgetId) {
-  grecaptcha.reset(widgetId);
-});
-
-const code = getCodeFromUserInput();
-confirmationResult.confirm(code).then((result) => {
-  // User signed in successfully.
-  const user = result.user;
-  // ...
-}).catch((error) => {
-  // User couldn't sign in (bad verification code?)
-  // ...
-});
-
-var credential = firebase.auth.PhoneAuthProvider.credential(confirmationResult.verificationId, code);
-
-firebase.auth().signInWithCredential(credential);
-
-import { GoogleAuthProvider } from "firebase/auth";
-
-const provider = new GoogleAuthProvider();
-
-provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-
-import { getAuth } from "firebase/auth";
-
-const auth = getAuth();
-auth.languageCode = 'it';
-// To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
-
-provider.setCustomParameters({
-  'login_hint': 'user@example.com'
-});
-
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-
-  import { getAuth, signInWithRedirect } from "firebase/auth";
-
-const auth = getAuth();
-signInWithRedirect(auth, provider);
-
-import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
-
-const auth = getAuth();
-getRedirectResult(auth)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access Google APIs.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-
-    // The signed-in user info.
-    const user = result.user;
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-
-  import { getAuth, signOut } from "firebase/auth";
-
-const auth = getAuth();
-signOut(auth).then(() => {
-  // Sign-out successful.
-}).catch((error) => {
-  // An error happened.
-});
-
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-
-  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-  import { getAuth, signOut } from "firebase/auth";
-
-  const auth = getAuth();
-  signOut(auth).then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-
-  import { getAuth, signInAnonymously } from "firebase/auth";
-
-const auth = getAuth();
-signInAnonymously(auth)
-  .then(() => {
-    // Signed in..
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-  });
-
-  import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
-
-import { GoogleAuthProvider } from "firebase/auth";
-
-const credential = GoogleAuthProvider.credential(
-  googleUser.getAuthResponse().id_token);
-
-  import { FacebookAuthProvider } from "firebase/auth";
-
-  const credential = FacebookAuthProvider.credential(
-    response.authResponse.accessToken);
-
-    import { EmailAuthProvider } from "firebase/auth";
-
-const credential = EmailAuthProvider.credential(email, password);
-
-import { getAuth, linkWithCredential } from "firebase/auth";
-
-const auth = getAuth();
-linkWithCredential(auth.currentUser, credential)
-  .then((usercred) => {
-    const user = usercred.user;
-    console.log("Anonymous account successfully upgraded", user);
-  }).catch((error) => {
-    console.log("Error upgrading anonymous account", error);
-  });
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
